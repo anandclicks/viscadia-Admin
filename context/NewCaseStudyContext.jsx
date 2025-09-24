a209e54df15b145c11169dd259f5c34e640021a9
Pusp anand <146056018+anandclicks@users.noreply.github.com>
2025-09-24 22:10:48 +0530

    Update NewCaseStudyContext.jsx

--- context/NewCaseStudyContext.jsx
+++ context/NewCaseStudyContext.jsx
@@ -1,152 +1,97 @@
 import axios from "axios";
-import toast from "react-hot-toast";
-
-
-// variable 
-export const eventPayload= {
-  logo: "", 
+import { createContext, useEffect, useState } from "react";
+import { uploadSingleImage } from "../src/utils/reuseableFunctions";
+const payload = {
+  urlTitle: "",
+  bannerImg: "",
+  img: "",
   title: "",
-  date: "",
-  location: "",
-  image: "",
-  headingImage: "",
-  heading: "",
-  subHeading: "",
-  speakerTopic: "",
-  speakerTime: "",
-  speakerDate: "",
-  sectionTwoStatus: 1,
-  sectionThreeStatus: 1,
-  sectionFourStatus: 1,
-  status: "draft",
-  speaker: [
-    {
-      fullName: "",
-      designation: "",
-      image: null,
-    },
-  ],
-  description: "",
-  forecastingSpecialists: [
-    {
-      image: null,
-      fullName: "",
-      designation: "",
-      introduction: "",
-    },
-  ],
-};
-export let webinarPayload = {
-  subHeading: "",
-  imageOne: null,
-  imageTwo: null,
-  keyPoints: [""],
-  webibarVideo: null,
-  status : "draft",
-  speaker: [
-    {
-      fullName: "",
-      designation: "",
-      image: null,
-    },
-  ],
+  texts: [""],
+  mainSubtitle: "",
+  challenges: [...Array(3).fill({ img: "", title: "", texts: [""] })],
+  approach: [{ title: "", texts: [] }],
+  outcomes: [...Array(3).fill({ img: "", title: "", texts: [""] })],
+  pdf: "",
+  status: 0,
 };
 
-export function sligGenerator(str) {
-  return str
-    .toString()
-    .normalize('NFKD')             
-    .replace(/[\u0300-\u036f]/g, '')
-    .toLowerCase()
-    .replace(/[^a-z0-9]+/g, '-') 
-    .replace(/^-+|-+$/g, '')
-    .replace(/-{2,}/g, '-');
-}
+export const NewCaseStudyContext = createContext({});
+export const NewCaseStudyContextProvider = ({ children }) => {
+  const [createCaseStudyData, setCreateStudyData] = useState({ ...payload });
 
-export function toSnakeCase(obj) {
-  if (Array.isArray(obj)) {
-    return obj.map(toSnakeCase);
-  } else if (obj !== null && typeof obj === "object") {
-    const newObj = {};
-    for (const key in obj) {
-      const snakeKey = key.replace(/([A-Z])/g, "_$1").toLowerCase();
-      newObj[snakeKey] = toSnakeCase(obj[key]);
-    }
-    return newObj;
-  }
-  return obj;
-}
-
-export function toCamelCase(obj) {
-  if (Array.isArray(obj)) {
-    return obj.map(toCamelCase);
-  } else if (obj !== null && typeof obj === "object") {
-    const newObj = {};
-    for (const key in obj) {
-      const camelKey = key.replace(/_([a-z])/g, (_, char) => char.toUpperCase());
-      newObj[camelKey] = toCamelCase(obj[key]);
-    }
-    return newObj;
-  }
-  return obj;
-}
-
-export const commonGetApiCall = async(endpoint)=>{
-  try {
-    const res = await axios.get(`http://54.219.242.41:4005/api/admin${endpoint}`)
-    return res.data
-  } catch (error) {
-    return error
-  }
-}
-
-export const putCommonApiForEvnts = async(endPoint,data)=>{
-  try {
-    const res = await axios.put(`http://54.219.242.41:4005/api/admin${endPoint}`,data)
-    return res.data
-  } catch (error) {
-    return error
-  }
-}
-
-export const uploadSingleImage = async (files) => {
-  if (!files?.length) return "";
-    const t = toast.loading("Please wait, uploading File...");
-  try {
-    const formData = new FormData();
-    let endPoint = files[0].type === "video/mp4" ? "form-files" : "single";
-    formData.append(files[0].type === "video/mp4" ? "additional_files" : "file",  files[0]);
-    const res = await axios.post(
-      `http://54.219.242.41:4005/api/upload/${endPoint}`,
-      formData,
-      {
-        headers: { "Content-Type": "multipart/form-data" },
+  const handleNewCaseStudyInputs = async (evt) => {
+    const { name, files, value, type } = evt.target;
+    if (type === "file" && files && files[0]) {
+      let res = await uploadSingleImage(files);
+      if (res) {
+        setCreateStudyData((prev) => ({ ...prev, [name]: res }));
       }
-    );
-    toast.dismiss(t);
-    if (res.data?.success) {
-      toast.success("File uploaded successfully!");
-      return files[0].type === "video/mp4" ? res.data?.data?.additional_files[0]?.url :res.data?.data?.url ;
+    } else {
+      setCreateStudyData((prev) => ({ ...prev, [name]: value }));
     }
-    toast.error(res.data?.message || "Upload failed");
-    return "Couldn't Upload";
-  } catch (err) {
-    toast.dismiss(t);
-    toast.error(err.response?.data?.message || err.message || "Upload error");
-    return "";
+  };
+
+  const functionForAddingPoints = (evt) => {
+    const { value } = evt.target;
+    setCreateStudyData((prev) => ({
+      ...prev,
+      texts: [...prev.texts, value],
+    }));
+  };
+
+  const handleKeyPointsChange = (evt, index) => {
+    let UpdatedKeyPoints = createCaseStudyData?.texts
+    UpdatedKeyPoints[index] = evt.target.value
+    setCreateStudyData((prev) => ({ ...prev, texts: UpdatedKeyPoints }))
   }
-};
 
-
-export const createEventApiCall = async (data) => {
-  let t = toast.loading("Creating Event..!");
-  try {
-    const res = await axios.post("http://54.219.242.41:4005/api/admin/events", data);
-    toast.dismiss(t)
-    return res.data; 
-  } catch (e) {
-    toast.dismiss(t)
-    return e;
+  const addObjsInSections = (name) => {
+    let arr = createCaseStudyData[name]
+    arr = [...arr, { img: "", title: "", texts: [] }]
+    setCreateStudyData((prev) => ({ ...prev, [name]: arr }))
+    console.log(createCaseStudyData);
   }
-};
 
+  const addObjsInSectionsTwo = () => {
+    let arr = createCaseStudyData?.approach
+    arr = [...arr, { title: "", texts: [] }]
+    setCreateStudyData((prev) => ({ ...prev, approach: arr }))
+  }
+
+  const handleObjInpusChanges = async (evt, arrName, index) => {
+    const { type, name, value, files } = evt.target
+    if (type === "file" && files && files[0]) {
+      const url = await uploadSingleImage(files)
+      if (url) {
+        setCreateStudyData((prev) => {
+          let arr = [...createCaseStudyData[arrName]]
+          arr[index] = {...arr[index],[name] : "tasting"}
+          return {...prev,[arrName] : arr}
+        })
+      }
+    }else {
+      setCreateStudyData((prev)=>{
+        let arr = createCaseStudyData[arrName]
+        arr[index] = {...arr[index],[name] : value}
+        return {...prev,[arrName] :arr }
+      })
+    }
+  }
+
+  useEffect(() => {
+    console.log(createCaseStudyData);
+  }, [createCaseStudyData])
+  return (
+    <NewCaseStudyContext.Provider value={{
+      handleNewCaseStudyInputs,
+      createCaseStudyData,
+      handleKeyPointsChange,
+      functionForAddingPoints,
+      addObjsInSections,
+      addObjsInSectionsTwo,
+      handleObjInpusChanges,
+    }}>
+      {children}
+    </NewCaseStudyContext.Provider>
+  );
+};
